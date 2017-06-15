fca44be49376f29b32838c5ac57ab45aaa329cbc
yuanmenglong886 <yuanmenglong886@163.com>
2017-06-15 18:22:41 +0800

    DEV：  第一次上传项目到仓库

---
+++ blog/Utils/fileUtils.js
@@ -0,0 +1,29 @@
+/**
+ * Created by a58 on 2017/6/12.
+ */
+var fs = require('fs');
+var path = require('path');
+function mkdir(dirpath,dirname){
+    //判断是否是第一次调用
+    if(typeof dirname === "undefined"){
+        if(fs.existsSync(dirpath)){
+            return;
+        }else{
+            mkdir(dirpath,path.dirname(dirpath));
+        }
+    }else{
+        //判断第二个参数是否正常，避免调用时传入错误参数
+        if(dirname !== path.dirname(dirpath)){
+            mkdir(dirpath);
+            return;
+        }
+        if(fs.existsSync(dirname)){
+            fs.mkdirSync(dirpath)
+        }else{
+            mkdir(dirname,path.dirname(dirname));
+            fs.mkdirSync(dirpath);
+        }
+    }
+    return dirpath+dirname;
+}
+module.exports = mkdir;
